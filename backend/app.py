46ecb6b26d82fa75d27fbff7fa6a78c48aeacfab
amirka7777 <amirmgoian0808@gmail.com>
2026-05-19 22:15:01 +0700

    123

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app, resources={r"/api/*": {"origins": "*"}})  
 
 
 app.config['JWT_SECRET_KEY'] = 'super-secret-key' 
